9d599091fb83b57b15ce66049b81d8da3e4159e4
ardkyer <k0207k@naver.com>
2024-05-19 18:32:57 +0900

    [ FEAT ] 정문, 김수환 , 광장 까지 클로킹 관련 코드 지우고 코드 간소화. 몹도 한마리씩 배치

--- GP_FinalProj/Globals.cpp
+++
@@ -1,6 +0,0 @@
-#include "Globals.h"
-
-SDL_Window* g_window = nullptr;
-SDL_Renderer* g_renderer = nullptr;
-bool g_flag_running = true;
-int g_current_game_phase = 0;
--- GP_FinalProj/StaticMonster.cpp
+++
@@ -1,7 +0,0 @@
-#include "StaticMonster.h"
-
-StaticMonster::StaticMonster(int x, int y) : Monster(x, y) {}
-
-void StaticMonster::Update(int playerX, int playerY) {
-    // StaticMonster는 위치를 업데이트하지 않습니다.
-}
