c1fdcd1b1a89bcb9bfea822825389a8ff0d5c199
고경우 <dfdfg11@gmail.com>
2024-06-03 22:52:26 +0900

    fix:플레이어 주변에 몹 안나오게 HAll까지

--- GP_FinalProj/Hall.cpp
+++ GP_FinalProj/Hall.cpp
@@ -12,6 +12,16 @@ void Hall::SpawnMonsters() {
     std::uniform_int_distribution<> disX(0, WINDOW_WIDTH - 128);
     std::uniform_int_distribution<> disY(0, WINDOW_HEIGHT - 128);
 
+    const int minDistanceFromPlayer = 200; // 플레이어로부터 최소 거리
+
+    auto generatePosition = [&](int& x, int& y) {
+        do {
+            x = disX(gen);
+            y = disY(gen);
+        } while (std::sqrt(std::pow(player_.GetRect().x - x, 2) + std::pow(player_.GetRect().y - y, 2)) < minDistanceFromPlayer);
+    };
+
+
 
     for (int i = 0; i < 4; ++i) {
         int x = disX(gen);
@@ -26,6 +36,14 @@ void Hall::SpawnMonster() {
     std::mt19937 gen(rd());
     std::uniform_int_distribution<> disX(0, WINDOW_WIDTH - 128);
     std::uniform_int_distribution<> disY(0, WINDOW_HEIGHT - 128);
+    const int minDistanceFromPlayer = 200; // 플레이어로부터 최소 거리
+
+    auto generatePosition = [&](int& x, int& y) {
+        do {
+            x = disX(gen);
+            y = disY(gen);
+        } while (std::sqrt(std::pow(player_.GetRect().x - x, 2) + std::pow(player_.GetRect().y - y, 2)) < minDistanceFromPlayer);
+    };
 
     if (monsters.size() < maxMonsters) {
 
@@ -33,4 +51,6 @@ void Hall::SpawnMonster() {
         int y = disY(gen);
         monsters.push_back(new ChargingMonster(x, y));
     }
+
+
 }
